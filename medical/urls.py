fbe0124ba966b5bb89360e8cac9d7e0b556fcc54
Sebas <sebastian@example.com>
2026-09-21 09:10:09 -0600

    Implementa módulo ClinicaFisioterapia

--- medical/urls.py
+++ medical/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path( "api/", include("clinica.urls"), ),
+    path( "api/fisioterapia/", include("clinica_fisioterapia.urls") ),
 ]
